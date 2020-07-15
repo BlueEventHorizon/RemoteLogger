94e6cc2aec8461b41818104fa69ff67a9c76b1b1
k2moons <moonsocean@gmail.com>
2020-07-15 21:06:50 +0900

    Add remote logger example app

---
+++ RemoteLoggerApp/RemoteLoggerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RemoteLoggerApp
+//
+//  Created by k_terada on 2020/07/15.
+//  Copyright © 2020 k2terada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
