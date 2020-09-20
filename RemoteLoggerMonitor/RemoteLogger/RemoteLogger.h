b77b6a23df4d4a3413f7da6ee1a7bfca4b8d2344
k2moons <moonsocean@gmail.com>
2020-09-20 23:47:45 +0900

    change: package to embedded framework for debugging

---
+++ RemoteLoggerMonitor/RemoteLogger/RemoteLogger.h
@@ -0,0 +1,19 @@
+//
+//  RemoteLogger.h
+//  RemoteLogger
+//
+//  Created by k_terada on 2020/09/20.
+//  Copyright © 2020 k2terada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RemoteLogger.
+FOUNDATION_EXPORT double RemoteLoggerVersionNumber;
+
+//! Project version string for RemoteLogger.
+FOUNDATION_EXPORT const unsigned char RemoteLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RemoteLogger/PublicHeader.h>
+
+
